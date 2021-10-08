50d61ad7ed1a515447383f14e5cfc13f12374035
mesomknf3 <72368636+mesomknf3@users.noreply.github.com>
2021-10-08 17:33:01 +0530

    Update heapsort.cpp

--- heapsort.cpp
+++ heapsort.cpp
@@ -59,7 +59,8 @@ int main()
 {
 	int arr[] = { 12, 11, 13, 5, 6, 7 };
 	int n = sizeof(arr) / sizeof(arr[0]);
-
+	cout<<53;
+	cout<<"Hacktoberfest2021";
 	heapSort(arr, n);
 
 	cout << "Sorted array is \n";
