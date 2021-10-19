0df38c8f14e2890cfe6000ee9a696cd1a27dcc17
Himanshu Chaurasia <72163817+Himacode@users.noreply.github.com>
2021-10-19 23:11:21 +0530

    Update heapsort.cpp

--- heapsort.cpp
+++ heapsort.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 // To heapify a subtree rooted with node i which is
 // an index in arr[]. n is size of heap
-void heapify(int arr[], int n, int i)
+void heapifyInRoot(int arr[], int n, int i)
 {
 	int largest = i; // Initialize largest as root
 	int l = 2 * i + 1; // left = 2*i + 1
@@ -27,7 +27,7 @@ void heapify(int arr[], int n, int i)
 		swap(arr[i], arr[largest]);
 
 		// Recursively heapify the affected sub-tree
-		heapify(arr, n, largest);
+		heapifyInRoot(arr, n, largest);
 	}
 }
 
@@ -36,7 +36,7 @@ void heapSort(int arr[], int nn)
 {
 	// Build heap (rearrange array)
 	for (int i = nn / 2 - 1; i >= 0; i--)
-		heapify(arr, nn, i);
+		heapifyInRoot(arr, nn, i);
 
 	// One by one extract an element from heap
 	for (int i = nn - 1; i > 0; i--) {
@@ -44,7 +44,7 @@ void heapSort(int arr[], int nn)
 		swap(arr[0], arr[i]);
 
 		// call max heapify on the reduced heap
-		heapify(arr, i, 0);
+		heapifyInRoot(arr, i, 0);
 	}
 }
 
