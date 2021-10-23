b82e6179a7f637da12c2b5868f7638ca134b010a
Juhisonkar <63336174+Juhisonkar@users.noreply.github.com>
2021-10-23 21:45:12 +0530

    Update heapsort.cpp

--- heapsort.cpp
+++ heapsort.cpp
@@ -11,17 +11,17 @@ using namespace std;
 void heapifyInRoot(int arr[], int n, int i)
 {
 	int largest = i; // Initialize largest as root
-	int l = 2 * i + 1; // left = 2*i + 1
-	int r = 2 * i + 2; // right = 2*i + 2
+	int p = 2 * i + 1; // left = 2*i + 1
+	int q = 2 * i + 2; // right = 2*i + 2
 
 	// If left child is larger than root
-	if (l < n && arr[l] > arr[largest])
+	if (p < n && arr[l] > arr[largest])
 		cout<<"And largest "
 		largest = l;
 	
 
 	// If right child is larger than largest so far
-	if (r < n && arr[r] > arr[largest])
+	if (q < n && arr[r] > arr[largest])
 		largest = r;
 
 	// If largest is not root
